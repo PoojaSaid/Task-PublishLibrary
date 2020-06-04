c0f480075e8b65b4498d4c5ecad5510f57f2b765
pooja <pooja.said28@gmail.com>
2020-06-04 17:51:17 +0530

    First

---
+++ scientificcalculator/src/main/java/com/example/scientificcalculator/Demo.java
@@ -0,0 +1,20 @@
+package com.example.scientificcalculator;
+
+public class Demo {
+    public float addition(float a, float b) {
+        return a + b;
+    }
+
+    public float subtraction(float a, float b) {
+        return a - b;
+    }
+
+    public float multiplication(float a, float b) {
+        return a * b;
+    }
+
+    public float division(float a, float b) {
+
+        return a / b;
+    }
+}
